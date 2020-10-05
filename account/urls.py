a06d7f9082a002bf63b32da1842b610662339896
rolbik <69362627+rolbik@users.noreply.github.com>
2020-10-05 19:01:05 +0200

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -9,6 +9,25 @@ urlpatterns = [
      path ('logout/', auth_views.LogoutView.as_view(), name='logout'),
      path ('register/', views.register, name='register'),
      path ('dashboard/', views.dashboard, name='dashboard'),
+     
+     # section of change and reset passwords at accounts
+     path ('password_change/', auth_views.PasswordChangeView.as_view(),
+                               name='password_change'),
+     path ('password_change/done/',
+           auth_views.PasswordChangeDoneView.as_view(),
+           name='password_change_done'),
+     path ('password_reset/',
+           auth_views.PasswordResetView.as_view(),
+           name='password_reset'),
+     path ('password_reset/done/',
+           auth_views.PasswordResetDoneView.as_view(),
+           name='password_reset_done'),
+     path ('reset/<uidb64>/<token>/',
+          auth_views.PasswordResetConfirmView.as_view(),
+          name='password_reset_confirm'),
+     path ('reset/done/',
+          auth_views.PasswordResetCompleteView.as_view(),
+          name='password_reset_complete'),
 
 
 ]
